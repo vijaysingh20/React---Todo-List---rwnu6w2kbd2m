27073d42a6b8490178cbefdbf11fab8ec5768f5b
Vijay <76722057+vijaysingh20@users.noreply.github.com>
2022-03-04 12:29:32 +0530

    Create Edit.js

---
+++ src/components/Edit.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Edit = (props) => {
+  return (
+    <div className="edit-box">
+      <textarea
+        className="editTask"
+        value={props.edited}
+        onChange={props.handleEditChange}
+        placeholder="Edit Task"
+      ></textarea>
+      <button
+        className="saveTask"
+        onClick={() => props.saveTask(props.editID)}
+        // disabled={props.edited.length === 0}
+      >
+        Save
+      </button>
+    </div>
+  );
+};
+
+export default Edit;
